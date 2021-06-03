1471e8cfbbc2c454ee4b70d2114b8b485557ec66
erfan <erfangholami76@gmail.com>
2021-06-03 12:42:03 +0430

    add BaseActivity to project

---
+++ app/src/main/java/com/greenwallet/business/scenes/base/BaseActivity.kt
@@ -0,0 +1,35 @@
+package com.greenwallet.business.scenes.base
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.greenwallet.business.R
+import com.greenwallet.business.scenes.login.LoginActivity
+
+open class BaseActivity: AppCompatActivity() {
+
+    companion object {
+        const val FRAGMENT_LOADING = "fragment_loading"
+    }
+
+    fun replaceFragment(targetFragment: Fragment, tag: String) {
+        val fragment = supportFragmentManager.findFragmentByTag(tag)
+        if (fragment == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, targetFragment, tag)
+                .addToBackStack(tag)
+                .commit()
+        } else if (!fragment.isVisible) {
+            supportFragmentManager.popBackStack(tag, 0)
+        }
+    }
+
+    fun userShouldReAuthenticate() {
+
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+        startActivity(intent)
+    }
+}
