02eea2f8071e016cb0b964ee8854e41e09a57dd8
Erfan Gholami <egholami@greenwallet.com>
2021-06-07 21:44:00 +0430

    add saved products to local database

--- app/src/main/java/com/greenwallet/business/helper/keystore/User.kt
+++ app/src/main/java/com/greenwallet/business/helper/keystore/User.kt
@@ -1,6 +1,9 @@
 package com.greenwallet.business.helper.keystore
 
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import com.greenwallet.business.app.Application.Companion.context
+import com.greenwallet.business.network.product.response.CartProduct
 
 class User {
 
@@ -11,7 +14,9 @@ class User {
         MERCHANT_ID,
         EMAIL,
         PASSWORD,
-        ACCESS_TOKEN,
+        CART_PRODUCT_LIST,
+        BUY_NOW_CART_PRODUCT_LIST,
+
     }
 
     companion object {
@@ -48,4 +53,235 @@ class User {
             encryptedPreference.putString(Keys.PASSWORD.toString(), value)
         }
 
+    var cartProducts: ArrayList<CartProduct> = arrayListOf()
+        get() {
+            return GsonBuilder().create().fromJson(
+                encryptedPreference.getString(Keys.CART_PRODUCT_LIST.toString(), "[]"),
+                object : TypeToken<ArrayList<CartProduct>>() {}.type
+            )
+        }
+        set(value) {
+            field = value
+
+            encryptedPreference.putString(
+                Keys.CART_PRODUCT_LIST.toString(),
+                GsonBuilder().create().toJson(value)
+            )
+        }
+
+    var buyNowCartProducts: ArrayList<CartProduct> = arrayListOf()
+        get() {
+            return GsonBuilder().create().fromJson(
+                encryptedPreference.getString(Keys.BUY_NOW_CART_PRODUCT_LIST.toString(), "[]"),
+                object : TypeToken<ArrayList<CartProduct>>() {}.type
+            )
+        }
+        set(value) {
+            field = value
+
+            encryptedPreference.putString(
+                Keys.BUY_NOW_CART_PRODUCT_LIST.toString(),
+                GsonBuilder().create().toJson(value)
+            )
+        }
+
+
+    //todo: after adding variations
+    /*
+    fun addProductToCart(cartProduct: CartProduct): Boolean {
+
+        val currentItems = cartProducts
+        var isNew = true
+
+        for (item in currentItems) {
+            if (item.product!!.productID == cartProduct.product!!.productID) {
+
+                if (item.variants.size == cartProduct.variants.size) {
+
+                    var sameVariantCount = 0
+                    for (itemVariant in item.variants) {
+                        for (cartVariant in cartProduct.variants) {
+                            if (itemVariant.type == cartVariant.type &&
+                                itemVariant.value == cartVariant.value
+                            ) {
+                                sameVariantCount++
+                            }
+                        }
+                    }
+
+                    if (sameVariantCount == item.variants.size) {
+                        item.product = cartProduct.product
+                        item.quantity += cartProduct.quantity
+                        item.timestamp = cartProduct.timestamp
+
+                        isNew = false
+                        break
+                    } else {
+                        isNew = true
+                    }
+                }
+            }
+        }
+
+        var returnValue = true
+        if (isNew) {
+            returnValue = currentItems.add(cartProduct)
+        }
+
+        cartProducts = currentItems
+        return returnValue
+    }
+
+    fun updateProductInCart(cartProduct: CartProduct) {
+
+        val currentItems = cartProducts
+
+        var itemIndex = -1
+        for ((index, item) in currentItems.withIndex()) {
+            if (item.product!!.productID == cartProduct.product!!.productID) {
+
+                if (item.variants.size == cartProduct.variants.size) {
+
+                    var sameVariantCount = 0
+                    for (itemVariant in item.variants) {
+                        for (cartVariant in cartProduct.variants) {
+                            if (itemVariant.type == cartVariant.type &&
+                                itemVariant.value == cartVariant.value
+                            ) {
+                                sameVariantCount++
+                            }
+                        }
+                    }
+                    if (sameVariantCount == item.variants.size) {
+                        //item for modification found
+                        itemIndex = index
+                        break
+                    } else {
+                        //continue searching
+                    }
+                }
+            }
+        }
+
+        if (itemIndex > -1) {
+            //item exists
+            val item = currentItems[itemIndex]
+            if (cartProduct.quantity == 0) {
+                currentItems.remove(item)
+            } else {
+                item.product = cartProduct.product
+                item.quantity = cartProduct.quantity
+                item.variants = cartProduct.variants
+                item.shipments = cartProduct.shipments
+                item.selectedShipmentMethod = cartProduct.selectedShipmentMethod
+                item.variation = cartProduct.variation
+                item.timestamp = cartProduct.timestamp
+                item.isBuyingNow = cartProduct.isBuyingNow
+            }
+        } else {
+            //item doesn't exists
+        }
+
+        cartProducts = currentItems
+    }
+
+    fun addProductToBuyNowCart(cartProduct: CartProduct): Boolean {
+
+        val currentItems = buyNowCartProducts
+        var isNew = true
+
+        for (item in currentItems) {
+            if (item.product!!.productID == cartProduct.product!!.productID) {
+
+                if (item.variants.size == cartProduct.variants.size) {
+
+                    var sameVariantCount = 0
+                    for (itemVariant in item.variants) {
+                        for (cartVariant in cartProduct.variants) {
+                            if (itemVariant.type == cartVariant.type &&
+                                itemVariant.value == cartVariant.value
+                            ) {
+                                sameVariantCount++
+                            }
+                        }
+                    }
+
+                    if (sameVariantCount == item.variants.size) {
+                        item.product = cartProduct.product
+                        item.quantity = 1
+                        item.timestamp = cartProduct.timestamp
+
+                        isNew = false
+                        break
+                    } else {
+                        isNew = true
+                    }
+                }
+            }
+        }
+
+        var returnValue = true
+        if (isNew) {
+            val newList = arrayListOf<CartProduct>()
+            returnValue = newList.add(cartProduct)
+            buyNowCartProducts = newList
+        } else {
+            buyNowCartProducts = currentItems
+        }
+
+        return returnValue
+    }
+
+    fun updateBuyNowProductInCart(cartProduct: CartProduct) {
+
+        val currentItems = buyNowCartProducts
+
+        var itemIndex = -1
+        for ((index, item) in currentItems.withIndex()) {
+            if (item.product!!.productID == cartProduct.product!!.productID) {
+
+                if (item.variants.size == cartProduct.variants.size) {
+
+                    var sameVariantCount = 0
+                    for (itemVariant in item.variants) {
+                        for (cartVariant in cartProduct.variants) {
+                            if (itemVariant.type == cartVariant.type &&
+                                itemVariant.value == cartVariant.value
+                            ) {
+                                sameVariantCount++
+                            }
+                        }
+                    }
+                    if (sameVariantCount == item.variants.size) {
+                        //item for modification found
+                        itemIndex = index
+                        break
+                    } else {
+                        //continue searching
+                    }
+                }
+            }
+        }
+
+        if (itemIndex > -1) {
+            //item exists
+            val item = currentItems[itemIndex]
+            if (cartProduct.quantity == 0) {
+                currentItems.remove(item)
+            } else {
+                item.product = cartProduct.product
+                item.quantity = cartProduct.quantity
+                item.variants = cartProduct.variants
+                item.shipments = cartProduct.shipments
+                item.selectedShipmentMethod = cartProduct.selectedShipmentMethod
+                item.variation = cartProduct.variation
+                item.timestamp = cartProduct.timestamp
+                item.isBuyingNow = cartProduct.isBuyingNow
+            }
+        } else {
+            //item doesn't exists
+        }
+
+        buyNowCartProducts = currentItems
+    } */
 }
---
+++ app/src/main/java/com/greenwallet/business/network/product/response/CartProduct.kt
@@ -0,0 +1,46 @@
+package com.greenwallet.business.network.product.response
+
+import java.util.*
+
+data class CartProduct(
+    var product: ProductResponseModel? = null,
+    var quantity: Int = 0,
+    //todo: after adding variations and shipments
+//    var variants: ArrayList<ProductVariantsResponseModel> = arrayListOf(),
+//    var shipments: ArrayList<ProductShipmentsResponseModel> = arrayListOf(),
+//    var selectedShipmentMethod: ProductShipmentsResponseModel? = null,
+//    var variation: ProductVariationsResponseModel? = null,
+    var timestamp: Long = Date().time,
+    var isBuyingNow: Boolean = false
+)
+
+fun CartProduct.getOldPrice(): Int {
+    return quantity * product!!.getOldPrice()
+}
+
+//todo: after adding variations
+/*fun CartProduct.getPrice(): Int {
+    return quantity *
+            if (product!!.isHotDeal()) {
+                product!!.getPrice()
+            } else {
+                if (variation == null || variation!!.price.amount == 0) {
+                    product!!.getPrice()
+                } else {
+                    variation!!.price.amount
+                }
+            }
+}
+
+fun CartProduct.getVat(): Int {
+    return quantity *
+            if (product!!.isHotDeal()) {
+                product!!.getVat()
+            } else {
+                if (variation == null || variation!!.price.amount == 0) {
+                    product!!.getVat()
+                } else {
+                    variation!!.price.vat
+                }
+            }
+}*/
